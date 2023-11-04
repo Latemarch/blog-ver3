2047a9b9296e664dd104665719ef36b48fbdd0aa
Latemarch <dolphin0331@naver.com>
2023-11-04 09:32:36 +0900

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
-  return <div className="flex justify-center w-full h-20 fixed z-10">haha</div>;
+  return (
+    <div className="flex justify-center w-full h-20 fixed z-10">
+      <Link href="/posts">Posts</Link>
+      <Link href="/projects">Projects</Link>
+      <Link href="/shorts">shorts</Link>
+    </div>
+  );
 }
--- test/Header.test.js
+++ test/Header.test.js
@@ -2,14 +2,14 @@ import { render } from "@testing-library/react";
 import Header from "../src/components/Header";
 
 describe("Header component", () => {
-  it('has justify-center style and contains text "haha"', () => {
-    const { getByText, container } = render(<Header />);
+  const { container } = render(<Header />);
 
-    // Check if the text 'haha' is present
-    expect(getByText("haha")).toBeInTheDocument();
+  it("contains expected links", () => {
+    const expectedHrefs = ["/posts", "/projects", "/shorts"];
 
-    // Check if it has the justify-center style from Tailwind
-    // This checks if the first child of the rendered component has the class 'justify-center'
-    expect(container.firstChild).toHaveClass("justify-center");
+    expectedHrefs.forEach((href) => {
+      const linkElement = container.querySelector(`a[href='${href}']`);
+      expect(linkElement).not.toBeNull();
+    });
   });
 });
